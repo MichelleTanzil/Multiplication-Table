1e3d43a1ac2aeaac5c94e96d64b672977cbac81d
Michelle Tanzil <mtanzil01@gmail.com>
2019-11-23 14:18:45 -0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] multiplicationTable = new int[10, 10];
+
+            for(int i = 1; i <= 10; i++)
+            {   
+                Console.Write("[");
+                for (int j = 1; j <= 10; j++) 
+                {
+                    Console.Write(i *j + ", ");
+                }
+                Console.Write("] \n");
+            }
+        }
+    }
+}
